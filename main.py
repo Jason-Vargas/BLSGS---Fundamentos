40aa64cdccd4d58bb64c998309b99b1699ce821b
Jason-Vargas <jason.vargas.jy@gmail.com>
2025-06-25 08:16:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from interfaz import *
 from login import ventana_login
 from musica import reproducir_musica
 
-#reproducir_musica()
+reproducir_musica()
 
 ventana_login(callback_al_loguear=interfaz)
